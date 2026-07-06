9af141b515864a9fb44d52975b5b3ca90b82d75f
Orlando Hernandez Celli <ohercelli@gmail.com>
2026-07-06 17:29:22 +0200

    Implement allowed_characters method in PDA
    Added method to retrieve allowed characters based on the current state of the PDA.

--- src/automata/pda.py
+++ src/automata/pda.py
@@ -49,6 +49,11 @@ class JSONPushdownAutomaton:
         self.current_key: str = ""
 
     def advance(self, char: str) -> bool:
+        """
+        Master router.
+        If active_fsm is alive, feed the char to the FSM.
+        If not, the PDA processes the structural char ({, }, [, ], :, ,)
+        """
         if self.active_fsm:
             return self._handle_fsm_input(char)
 
@@ -62,3 +67,38 @@ class JSONPushdownAutomaton:
 
     def _handle_structural_input(self, char: str) -> bool:
         return False
+        
+    def allowed_characters(self) -> frozenset[str] | set[str]:
+        """
+        If active_fsm is alive, return the FSM's allowed chars.
+        Otherwise, return the PDA's allowed structural chars.
+        """
+        if self.active_fsm:
+            return self.active_fsm.allowed_characters()
+
+        match self.state:
+            case PDAState.TERMINAL:
+                return self.CHARS_EMPTY
+
+            case PDAState.EXPECTING_OBJECT_START:
+                return self.CHAR_OBJECT_START 
+
+            case PDAState.EXPECTING_KEY:
+                return self.CHAR_KEY_QUOTE
+
+            case PDAState.EXPECTING_COLON:
+                return self.CHAR_COLON
+
+            case PDAState.EXPECTING_ARRAY_START:
+                return self.CHAR_ARRAY_START
+
+            case PDAState.EXPECTING_COMMA_OR_END:
+                if self.stack[-1] == Scope.OBJECT:
+                    return self.CHARS_OBJECT_NEXT
+                elif self.stack[-1] == Scope.ARRAY:
+                    return self.CHARS_ARRAY_NEXT
+
+            case _:
+                return self.CHARS_EMPTY
+
+        return self.CHARS_EMPTY
