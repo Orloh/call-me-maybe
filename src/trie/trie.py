734217239be1e95081c5444ed5fe06ecc4a60496
Orloh <ohercelli@gmail.com>
2026-07-07 21:29:20 +0200

    feat(trie): implement character by character insertion and memory sharing logic

--- src/trie/trie.py
+++ src/trie/trie.py
@@ -18,12 +18,25 @@ class PrefixTrie:
 
     def insert(self, token_str: str, token_id: int) -> None:
         """
-        Inserts a token string and tis correspondint ID into the Trie.
+        Inserts a token string and its corresponding ID into the Trie.
         """
-        pass
+        if not token_str:
+            return
+
+        current_node = self.root
+
+        for char in token_str:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+            
+            current_node = current_node.children[char]
+
+        if current_node.token_id is None:
+            current_node.token_id = token_id
+            self.size += 1
 
     def build_from_vocabulary(self, vocabulary: dict[str, int]) -> None:
         """
-        Utility method to ingest a massive dictionary of tokens in IDs all at once.
+        Utility method to ingest a massive dictionary of tokens and IDs all at once.
         """
         pass
