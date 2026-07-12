4bbe82374aaa49539e7c563a9a634b9f5b37f48a
Orloh <ohercelli@gmail.com>
2026-07-12 21:42:00 +0200

    feat(engine): add constructor to pipline class

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -1,5 +1,11 @@
+from .bootstrap import initialize_system_dependencies
 from .generator import ConstrainedGenerator
+from .prompt_builder import PromptBuilder
+from .pipeline import FunctionCallingPipeline
 
 __all__ = [
-    "ConstrainedGenerator"
+    "ConstrainedGenerator",
+    "PromptBuilder",
+    "FunctionCallingPipeline",
+    "initialize_system_dependencies"
 ]
