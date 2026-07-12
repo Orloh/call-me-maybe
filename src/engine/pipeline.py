ac95796a0600752bc76af60a63399fd2e1dbd5dc
Orloh <ohercelli@gmail.com>
2026-07-12 22:24:20 +0200

    feat(engine): update pipline constructor

--- src/engine/pipeline.py
+++ src/engine/pipeline.py
@@ -1,10 +1,11 @@
 import json
 import logging
-from src.schema import FunctionDefinition, FunctionCallResult
-from src.automata import JSONPushdownAutomaton, SchemaCompiler
-from src.engine import ConstrainedGenerator, PromptBuilder
+
 from llm_sdk import Small_LLM_Model
 from src.trie import PrefixTrie
+from src.automata import JSONPushdownAutomaton, CompiledSchema, compile_tools
+from src.schema import FunctionDefinition, FunctionCallResult
+from src.engine import ConstrainedGenerator, PromptBuilder 
 
 logger = logging.getLogger(__name__)
 
@@ -12,4 +13,15 @@ class FunctionCallingPipeline:
     """
     Orchestrates the two-phase generation process for single prompt.
     """
-
+    def __init__(
+        self,
+        model: Small_LLM_Model,
+        trie: PrefixTrie,
+        available_functions: list[FunctionDefinition]
+    ):
+        self.model = model
+        self.trie = trie
+        self.extractor_table = compile_tools(available_functions)
+        self.router_table = {
+            "name": self.extractor_table["name"]
+        }
