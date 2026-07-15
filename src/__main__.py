99a6213ad1a71cd8bbbca25bd216072eb0443939
Orloh <ohercelli@gmail.com>
2026-07-15 07:31:34 +0200

    feat(feat): wire up bootstrap and io_manager into main script

--- src/__main__.py
+++ src/__main__.py
@@ -10,13 +10,25 @@ logging_format = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=logging.INFO, format=logging_format)
 logger = logging.getLogger(__name__)
 
-def main():
+def main() -> None:
     # Path Setup
-    # Bootstrapping
-    # Load Data Using io_manager
-    # Execute pipline
-    # Save Output using io_manager
-    pass
+    project_root = Path(__file__).resolve().parent.parent
+    input_dir = project_root / "data" / "input"
+    output_dir = project_root / "data" / "output"
 
-if __name__ == "__main__":
-    main()
+    functions_path = input_dir / "function_definitions" / "function_definitions.json"
+    prompts_path = input_dir / "function_call_prompts" / "function_calling_tests.json"
+    output_path = output_dir / "function_calling_results.json"
+
+    logger.info(f"{project_root} :{project_root.exists()}")
+
+    # Bootstrapping
+    logger.info("Bootstrapping dependencies (Model & Prefix Trie)...")
+    model, trie = initialize_system_dependencies()
+
+    # Load Data Using io_manager
+    logger.info("Loading function definitions and prompts...")
+    available_functions = load_function_definitions(functions_path)
+    prompt_items = load_prompts(prompts_path)
+
+    # Execute pipline
